3ea8cded1b6c89f5856e121d158a3ab275443cba
Charlotte Heegaard <chhe@zealand.dk>
2019-11-19 09:05:33 +0100

    x

---
+++ App1/Properties/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Properties
+{
+    class Class2
+    {
+        /*test*/
+        /*Test2*/
+    }
+}
